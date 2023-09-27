37a9d55a511fffe14d39412c0f16d4c1ff186b74
github-action-benchmark <github@users.noreply.github.com>
2023-09-27 01:28:07 +0000

    add GAHM Benchmark (googlecpp) benchmark result for 1148493fd45e26be123cafdde890de544e21ea3d

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1695778087899,
+  "repoUrl": "https://github.com/adcirc/gahm",
+  "entries": {
+    "GAHM Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "zcobell@gmail.com",
+            "name": "Zach Cobell",
+            "username": "zcobell"
+          },
+          "committer": {
+            "email": "zcobell@gmail.com",
+            "name": "Zach Cobell",
+            "username": "zcobell"
+          },
+          "distinct": true,
+          "id": "1148493fd45e26be123cafdde890de544e21ea3d",
+          "message": "Adding benchmark workflow",
+          "timestamp": "2023-09-26T21:16:04-04:00",
+          "tree_id": "8d29b630585013488f96ebcf60552dd444a5e40b",
+          "url": "https://github.com/adcirc/gahm/commit/1148493fd45e26be123cafdde890de544e21ea3d"
+        },
+        "date": 1695778087055,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_Vortex",
+            "value": 48803051.307693034,
+            "unit": "ns/iter",
+            "extra": "iterations: 13\ncpu: 48768515.384615384 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
