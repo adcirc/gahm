2492c5877e5df6fbbda1745f04c192ad96d5fcfa
Zach Cobell <zcobell@gmail.com>
2024-03-13 01:39:16 -0400

    Modernizing Date class

--- src/datatypes/Date.h
+++ src/datatypes/Date.h
@@ -38,20 +38,28 @@
 namespace Gahm::Datatypes {
 class Date {
  public:
+  static constexpr int hours_per_day = 24;
+  static constexpr int days_per_week = 7;
+  static constexpr int months_per_year = 12;
+  static constexpr int days_per_year_numerator = 146097;
+  static constexpr int days_per_year_denominator = 400;
+
   using milliseconds = std::chrono::milliseconds;
   using seconds = std::chrono::seconds;
   using minutes = std::chrono::minutes;
   using hours = std::chrono::hours;
-  using days = std::chrono::duration<
-      int, std::ratio_multiply<std::ratio<24>, std::chrono::hours::period>>;
-  using weeks =
+  using days =
       std::chrono::duration<int,
-                            std::ratio_multiply<std::ratio<7>, days::period>>;
+                            std::ratio_multiply<std::ratio<hours_per_day>,
+                                                std::chrono::hours::period>>;
+  using weeks = std::chrono::duration<
+      int, std::ratio_multiply<std::ratio<days_per_week>, days::period>>;
   using years = std::chrono::duration<
-      int, std::ratio_multiply<std::ratio<146097, 400>, days::period>>;
-  using months =
-      std::chrono::duration<int,
-                            std::ratio_divide<years::period, std::ratio<12>>>;
+      int, std::ratio_multiply<
+               std::ratio<days_per_year_numerator, days_per_year_denominator>,
+               days::period>>;
+  using months = std::chrono::duration<
+      int, std::ratio_divide<years::period, std::ratio<months_per_year>>>;
 
   Date();
 
@@ -69,71 +77,67 @@ class Date {
 
   //...operator overloads
 #ifndef SWIG
-  bool operator<(const Date &d) const;
-  bool operator>(const Date &d) const;
-  bool operator<=(const Date &d) const;
-  bool operator>=(const Date &d) const;
-  bool operator==(const Date &d) const;
-  bool operator!=(const Date &d) const;
-  Date &operator=(const Date &d) = default;
+  auto operator<(const Date &d) const -> bool;
+  auto operator>(const Date &d) const -> bool;
+  auto operator<=(const Date &d) const -> bool;
+  auto operator>=(const Date &d) const -> bool;
+  auto operator==(const Date &d) const -> bool;
+  auto operator!=(const Date &d) const -> bool;
+  auto operator=(const Date &d) -> Date & = default;
 
-  template <class T, typename std::enable_if<std::is_integral<T>::value>::type
-                         * = nullptr>
-  Date &operator+=(const T &rhs) {
+  template <class T, std::enable_if_t<std::is_integral_v<T>> * = nullptr>
+  auto operator+=(const T &rhs) -> Date & {
     this->m_datetime += Date::seconds(rhs);
     return *this;
   }
 
-  template <class T, typename std::enable_if<
-                         std::is_floating_point<T>::value>::type * = nullptr>
-  Date &operator+=(const T &rhs) {
-    this->m_datetime +=
-        Date::milliseconds(static_cast<long>(std::floor(rhs * 1000.0)));
+  template <class T, std::enable_if_t<std::is_floating_point_v<T>> * = nullptr>
+  auto operator+=(const T &rhs) -> Date & {
+    constexpr int milliseconds_per_second = 1000;
+    this->m_datetime += Date::milliseconds(
+        static_cast<long>(std::floor(rhs * milliseconds_per_second)));
     return *this;
   }
 
-  template <class T,
-            typename std::enable_if<!std::is_integral<T>::value &&
-                                    !std::is_floating_point<T>::value &&
-                                    !std::is_same<T, Date::years>::value &&
-                                    !std::is_same<T, Date::months>::value>::type
-                * = nullptr>
-  Date &operator+=(const T &rhs) {
+  template <
+      class T,
+      std::enable_if_t<!std::is_integral_v<T> && !std::is_floating_point_v<T> &&
+                       !std::is_same_v<T, Date::years> &&
+                       !std::is_same_v<T, Date::months>> * = nullptr>
+  auto operator+=(const T &rhs) -> Date & {
     this->m_datetime += rhs;
     return *this;
   }
 
-  Date &operator+=(const Date::years &rhs);
-  Date &operator+=(const Date::months &rhs);
+  auto operator+=(const Date::years &rhs) -> Date &;
+  auto operator+=(const Date::months &rhs) -> Date &;
 
-  template <class T, typename std::enable_if<std::is_integral<T>::value>::type
-                         * = nullptr>
-  Date &operator-=(const T &rhs) {
+  template <class T, std::enable_if_t<std::is_integral_v<T>> * = nullptr>
+  auto operator-=(const T &rhs) -> Date & {
     this->m_datetime -= Date::seconds(rhs);
     return *this;
   }
 
-  template <class T, typename std::enable_if<
-                         std::is_floating_point<T>::value>::type * = nullptr>
-  Date &operator-=(const T &rhs) {
-    this->m_datetime -=
-        Date::milliseconds(static_cast<long>(std::floor(rhs * 1000.0)));
+  template <class T, std::enable_if_t<std::is_floating_point_v<T>> * = nullptr>
+  auto operator-=(const T &rhs) -> Date & {
+    constexpr int milliseconds_per_second = 1000;
+    this->m_datetime -= Date::milliseconds(
+        static_cast<long>(std::floor(rhs * milliseconds_per_second)));
     return *this;
   }
 
-  template <class T,
-            typename std::enable_if<!std::is_integral<T>::value &&
-                                    !std::is_floating_point<T>::value &&
-                                    !std::is_same<T, Date::years>::value &&
-                                    !std::is_same<T, Date::months>::value>::type
-                * = nullptr>
-  Date &operator-=(const T &rhs) {
+  template <
+      class T,
+      std::enable_if_t<!std::is_integral_v<T> && !std::is_floating_point_v<T> &&
+                       !std::is_same_v<T, Date::years> &&
+                       !std::is_same_v<T, Date::months>> * = nullptr>
+  auto operator-=(const T &rhs) -> Date & {
     this->m_datetime -= rhs;
     return *this;
   }
 
-  Date &operator-=(const Date::years &rhs);
-  Date &operator-=(const Date::months &rhs);
+  auto operator-=(const Date::years &rhs) -> Date &;
+  auto operator-=(const Date::months &rhs) -> Date &;
 
 #endif
 
@@ -145,10 +149,16 @@ class Date {
   void addMonths(const long &value);
   void addYears(const long &value);
 
-  static Date maxDate() { return Date(3000, 1, 1, 0, 0, 0); }
-  static Date minDate() { return Date(1900, 1, 1, 0, 0, 0); }
+  static auto maxDate() -> Date {
+    constexpr int max_year = 3000;
+    return Date(max_year, 1, 1, 0, 0, 0);
+  }
+  static auto minDate() -> Date {
+    constexpr int min_year = 1900;
+    return Date(min_year, 1, 1, 0, 0, 0);
+  }
 
-  NODISCARD std::vector<long long> get() const;
+  NODISCARD auto get() const -> std::vector<long long>;
 
   void set(const std::vector<long long> &v);
   void set(const std::chrono::system_clock::time_point &t);
@@ -157,65 +167,66 @@ class Date {
            long long minute = 0, long long second = 0,
            long long millisecond = 0);
 
-  static Date fromSeconds(long seconds);
+  static auto fromSeconds(long seconds) -> Date;
 
   void fromMSeconds(long long mseconds);
 
-  NODISCARD long toSeconds() const;
+  NODISCARD auto toSeconds() const -> long;
 
-  NODISCARD long long toMSeconds() const;
+  NODISCARD auto toMSeconds() const -> long long;
 
-  NODISCARD int year() const;
+  NODISCARD auto year() const -> int;
   void setYear(int year);
 
-  NODISCARD unsigned month() const;
+  NODISCARD auto month() const -> unsigned;
   void setMonth(unsigned month);
 
-  NODISCARD unsigned day() const;
+  NODISCARD auto day() const -> unsigned;
   void setDay(unsigned day);
 
-  NODISCARD long long hour() const;
+  NODISCARD auto hour() const -> long long;
   void setHour(long long hour);
 
-  NODISCARD long long minute() const;
+  NODISCARD auto minute() const -> long long;
   void setMinute(long long minute);
 
-  NODISCARD long long second() const;
+  NODISCARD auto second() const -> long long;
   void setSecond(long long second);
 
-  NODISCARD long long millisecond() const;
+  NODISCARD auto millisecond() const -> long long;
   void setMillisecond(long long milliseconds);
 
-  NODISCARD static Date fromString(
+  NODISCARD static auto fromString(
       const std::string &datestr,
-      const std::string &format = "%Y-%m-%d %H:%M:%OS");
+      const std::string &format = "%Y-%m-%d %H:%M:%OS") -> Date;
 
-  NODISCARD std::string toString(
-      const std::string &format = "%Y-%m-%d %H:%M:%OS") const;
+  NODISCARD auto toString(
+      const std::string &format = "%Y-%m-%d %H:%M:%OS") const -> std::string;
 
-  NODISCARD std::chrono::system_clock::time_point time_point() const;
+  NODISCARD auto time_point() const -> std::chrono::system_clock::time_point;
 
-  static Date now();
+  static auto now() -> Date;
 
  private:
   std::chrono::system_clock::time_point m_datetime;
 };
 
 template <typename T>
-Date operator+(Date lhs, const T &rhs) {
+auto operator+(Date lhs, const T &rhs) -> Date {
   lhs += rhs;
   return lhs;
 }
 
 template <typename T>
-Date operator-(Date lhs, const T &rhs) {
+auto operator-(Date lhs, const T &rhs) -> Date {
   lhs -= rhs;
   return lhs;
 }
 }  // namespace Gahm::Datatypes
 
 #ifndef SWIG
-std::ostream &operator<<(std::ostream &os, const Gahm::Datatypes::Date &dt);
+auto operator<<(std::ostream &os, const Gahm::Datatypes::Date &dt)
+    -> std::ostream &;
 #endif
 
 #endif  // GAHMDATE_H
